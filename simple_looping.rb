e7df6b0457ad2c2764f7c49a4f619ea10d4ff9ab
John Grubbs <johnmlowery@gmail.com>
2018-08-15 17:15:14 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -41,6 +41,6 @@ end
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   number_of_times = 1..7
-  for 1..7 do 
+  for number_of_times do  
 end
 
